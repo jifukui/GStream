964c5d8240488629ab69a33afc4b5851a4535cc4
jifukui <805013833@qq.com>
2019-06-17 17:13:36 +0800

    编译版本

---
+++ _stdint.h
@@ -0,0 +1,9 @@
+#ifndef _GSTREAMER__STDINT_H
+#define _GSTREAMER__STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "gstreamer 1.16.0"
+/* generated using gnu compiler gcc (GCC) 4.8.5 20150623 (Red Hat 4.8.5-16) */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
